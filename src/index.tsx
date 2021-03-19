bcf86618260271b09c639ba9d441912ccccf9c36
Kağan Uğur <kgnugur@gmail.com>
2021-03-19 11:43:23 +0300

    [12] Version update, cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,3 @@
-import { HTMLAttributes, ReactChild } from 'react';
-
-export interface Props extends HTMLAttributes<HTMLDivElement> {
-  children?: ReactChild;
-}
-
-// Please do not use types off of a default export module or else Storybook Docs will suffer.
-// see: https://github.com/storybookjs/storybook/issues/9556
-
 export * from './form-checkbox';
 export * from './input-control';
 export * from './number-input-control';
