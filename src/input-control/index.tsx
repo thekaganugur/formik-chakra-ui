e1b166c31eb8608c6108e24d3930a63ea674dc42
Kağan Uğur <kgnugur@gmail.com>
2021-10-15 08:42:22 +0300

    Fixes inputcontrol ref forwarding

--- src/input-control/index.tsx
+++ src/input-control/index.tsx
@@ -1,21 +1,21 @@
 import { Input, InputProps } from '@chakra-ui/react';
 import { useField } from 'formik';
-import React, { FC } from 'react';
+import React, { FC, ForwardedRef } from 'react';
 import { BaseProps, FormControl } from '../form-control';
 
 export type InputControlProps = BaseProps & { inputProps?: InputProps };
 
-export const InputControl: FC<InputControlProps> = (
-  props: InputControlProps
-) => {
-  const { name, label, inputProps, ...rest } = props;
-  const [field] = useField(name);
+export const InputControl: FC<InputControlProps> = React.forwardRef(
+  (props: InputControlProps, ref: ForwardedRef<HTMLInputElement>) => {
+    const { name, label, inputProps, ...rest } = props;
+    const [field] = useField(name);
 
-  return (
-    <FormControl name={name} label={label} {...rest}>
-      <Input {...field} id={name} {...inputProps} />
-    </FormControl>
-  );
-};
+    return (
+      <FormControl name={name} label={label} {...rest}>
+        <Input {...field} id={name} {...inputProps} ref={ref} />
+      </FormControl>
+    );
+  }
+);
 
 export default InputControl;
